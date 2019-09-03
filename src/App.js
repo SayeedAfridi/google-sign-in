e5d0dc009c537cc2025fd0ceae7fb39795d19b50
Sayeed Afridi <as.afridi563@gmail.com>
2019-09-04 02:35:24 +0600

    conditional rendering on button

--- src/App.js
+++ src/App.js
@@ -16,10 +16,9 @@ class App extends Component {
 
   
   responseGoogle = (response) => {
-    // console.log(response);
-    // console.log(response.w3.U3);
-    // console.log(response.profileObj);
-    if(response.w3.U3){
+    if(response.error){
+      console.log(response.error);
+    }else if(response.w3.U3){
       this.setState({login: true});
       this.setState({profile: response.profileObj});
     }
@@ -30,7 +29,16 @@ class App extends Component {
   render(){
     const { login, profile} = this.state;
     let rep = <p>Your login will appear here.</p>;
+    let button;
     if(login === true){
+      button = (
+        <GoogleLogout
+            clientId="1073306259932-4dqh1i9lk9l3rr5msm52aausp1ecffhh.apps.googleusercontent.com"
+            buttonText="Sign out"
+            onLogoutSuccess={this.logout}
+          >
+        </GoogleLogout>
+      )
       rep = (
         <div>
             <h2>{profile.name}</h2>
@@ -39,6 +47,15 @@ class App extends Component {
         </div>
       )
     }else{
+      button = (
+        <GoogleLogin
+            clientId="1073306259932-4dqh1i9lk9l3rr5msm52aausp1ecffhh.apps.googleusercontent.com"
+            buttonText="Sign in with Google"
+            onSuccess={this.responseGoogle}
+            onFailure={this.responseGoogle}
+            cookiePolicy={'single_host_origin'}
+        />
+      )
       rep = <p>Your login will appear here.</p>
     }
     
@@ -47,20 +64,9 @@ class App extends Component {
         <Card className='card'>
           <h1>Sign in With Google</h1>
           <p>This is an api Test app. To test  Click the Button</p>
-          <GoogleLogin
-            clientId="1073306259932-4dqh1i9lk9l3rr5msm52aausp1ecffhh.apps.googleusercontent.com"
-            buttonText="Sign in with Google"
-            onSuccess={this.responseGoogle}
-            onFailure={this.responseGoogle}
-            cookiePolicy={'single_host_origin'}
-          />
-          <GoogleLogout
-            clientId="1073306259932-4dqh1i9lk9l3rr5msm52aausp1ecffhh.apps.googleusercontent.com"
-            buttonText="Sign out"
-            onLogoutSuccess={this.logout}
-          >
-          </GoogleLogout>
-          <p>{rep}</p>
+          {button}
+          
+          {rep}
          
         </Card>
       </Container>
