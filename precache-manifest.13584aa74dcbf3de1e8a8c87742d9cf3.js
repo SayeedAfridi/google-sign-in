01860b9ad0e93f34457208791d5d4f33a55ce9ff
Sayeed Afridi <as.afridi563@gmail.com>
2019-09-04 00:35:58 +0600

    Updates

---
+++ precache-manifest.13584aa74dcbf3de1e8a8c87742d9cf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ef1a4c33cf018fe5a60280dbd734a7",
+    "url": "/google-sign-in/index.html"
+  },
+  {
+    "revision": "6f7c7704f97a4416375d",
+    "url": "/google-sign-in/static/css/main.c816482f.chunk.css"
+  },
+  {
+    "revision": "4a3a0f1eba64c4baac12",
+    "url": "/google-sign-in/static/js/2.811c5e1b.chunk.js"
+  },
+  {
+    "revision": "6f7c7704f97a4416375d",
+    "url": "/google-sign-in/static/js/main.11f0fc60.chunk.js"
+  },
+  {
+    "revision": "e594506d34f81d1f8cb3",
+    "url": "/google-sign-in/static/js/runtime~main.46181832.js"
+  }
+]);
