c65cb4b19d6aaa5e4712ed9bdbc129f349e50792
Sayeed Afridi <as.afridi563@gmail.com>
2019-09-04 00:34:38 +0600

    added ui and api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import GoogleLogin from 'react-google-login'
 import './App.css';
+import { Container, Card } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+  responseGoogle = (response) => {
+    console.log(response);
+  }
+  render(){
+    return( 
+      <Container maxWidth='sm'>
+        <Card className='card'>
+          <h1>Sign in With Google</h1>
+          <p>This is an api Test app. To test  Click the Button</p>
+          <GoogleLogin
+            clientId="1073306259932-4dqh1i9lk9l3rr5msm52aausp1ecffhh.apps.googleusercontent.com"
+            buttonText="Sign in with Google"
+            onSuccess={this.responseGoogle}
+            onFailure={this.responseGoogle}
+            cookiePolicy={'single_host_origin'}
+          />
+        </Card>
+      </Container>
+    )
+  }
 }
 
 export default App;
