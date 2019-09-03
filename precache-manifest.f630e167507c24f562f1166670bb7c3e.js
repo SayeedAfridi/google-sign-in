ddb7425f8b6de2785ba36e2d405637059bd0da07
Sayeed Afridi <as.afridi563@gmail.com>
2019-09-04 01:58:12 +0600

    Updates

---
+++ precache-manifest.f630e167507c24f562f1166670bb7c3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2dc56e283237ca6bc8b542326a7d54",
+    "url": "/google-sign-in/index.html"
+  },
+  {
+    "revision": "78e49e8a0eb72eb123ef",
+    "url": "/google-sign-in/static/css/main.4624dadc.chunk.css"
+  },
+  {
+    "revision": "a098564daecb9a129480",
+    "url": "/google-sign-in/static/js/2.7cf4760a.chunk.js"
+  },
+  {
+    "revision": "78e49e8a0eb72eb123ef",
+    "url": "/google-sign-in/static/js/main.099374c8.chunk.js"
+  },
+  {
+    "revision": "e594506d34f81d1f8cb3",
+    "url": "/google-sign-in/static/js/runtime~main.46181832.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/google-sign-in/precache-manifest.218c73530d64c53767869de25b23fa59.js"
+  "/google-sign-in/precache-manifest.f630e167507c24f562f1166670bb7c3e.js"
 );
 
 self.addEventListener('message', (event) => {
